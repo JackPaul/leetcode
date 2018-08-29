c93f7c4dd950a8f4d80e6755e06972eed02af410
Jack Paul <jackpaul.bupt@gmail.com>
2018-08-30 07:30:15 +0800

    ...

--- Leetcode/src/easy/P572.java
+++ Leetcode/src/easy/P572.java
@@ -2,20 +2,20 @@ package easy;
 
 public class P572 {
 
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-        if(s == null && t != null)
-        	return false;
-        if(s == null && t == null)
-        	return true;
-        if(s != null && t == null)
-        	return false;
-        
-        if(s.val == t.val)
-        	return (isSubtree(s.left, t) || isSubtree(s.right, t)) || isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
-        else
-        	return isSubtree(s.left, t) || isSubtree(s.right, t);
-    }
-    
+//    public boolean isSubtree(TreeNode s, TreeNode t) {
+//        if(s == null && t != null)
+//        	return false;
+//        if(s == null && t == null)
+//        	return true;
+//        if(s != null && t == null)
+//        	return false;
+//        
+//        if(s.val == t.val)
+//        	return (isSubtree(s.left, t) || isSubtree(s.right, t)) || isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+//        else
+//        	return isSubtree(s.left, t) || isSubtree(s.right, t);
+//    }
+//    
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null)
         	return true;
@@ -28,5 +28,17 @@ public class P572 {
         } else {
         	return false;
         }
+    }
+    
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+    	if(s == null && t == null)
+    		return true;
+    	if(s != null && t != null) {
+    		if(s.val == t.val)
+    			return isSameTree(s.left, t.left) && isSameTree(s.right, t.right) || isSubtree(s.left, t) && isSubtree(s.right, t);;
+    	} if(s != null) {
+    		return isSubtree(s.left, t) && isSubtree(s.right, t);
+    	} else
+    		return false;
     }
 }
