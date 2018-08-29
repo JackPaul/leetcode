1a08c82424e66ec2d21976f6f33519506be59123
Jack Paul <jackpaul.bupt@gmail.com>
2018-08-29 19:12:09 +0800

    437

--- Leetcode/src/easy/P437.java
+++ Leetcode/src/easy/P437.java
@@ -5,14 +5,59 @@ public class P437 {
     public int pathSum(TreeNode root, int sum) {
         if(root == null)
         	return 0;
-    	return hasSum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+        
+        if(root.val == sum) {
+        	return 1 + pathSum(root.left, sum) + pathSum(root.right, sum)
+        	+ pathTotal(root.left, sum-root.val) + pathTotal(root.right, sum-root.val);
+        } else {
+        	return pathSum(root.left, sum) + pathSum(root.right, sum)
+        	+ pathTotal(root.left, sum-root.val) + pathTotal(root.right, sum-root.val);
+        }
+//        if(root.left == null && root.right == null) {
+//        	if(root.val == sum)
+//        		return 1;
+//        	return 0;
+//        } else if(root.left != null && root.right != null) {
+//            if(root.val == sum)
+//            	return 1 + pathSum(root.left, sum) + pathSum(root.right, sum) 
+//            	+ pathSum(root.left, sum-root.val) + pathSum(root.right, sum-root.val);
+//            else
+//            	return pathSum(root.left, sum) + pathSum(root.right, sum) 
+//            	+ pathSum(root.left, sum-root.val) + pathSum(root.right, sum-root.val);
+//        } else if(root.right != null) {
+//            if(root.val == sum)
+//            	return 1 + pathSum(root.right, sum) 
+//            	+ pathSum(root.right, sum-root.val);
+//            else
+//            	return pathSum(root.right, sum) 
+//            	+ pathSum(root.right, sum-root.val);
+//        } else {
+//            if(root.val == sum)
+//            	return 1 + pathSum(root.left, sum)
+//            	+ pathSum(root.left, sum-root.val);
+//            else
+//            	return pathSum(root.left, sum)  
+//            	+ pathSum(root.left, sum-root.val);
+//        }
+        
     }
+
+	private int pathTotal(TreeNode root, int target) {
+		if(root == null)
+			return 0;
+		if(root.val == target)
+			return 1 + pathTotal(root.left, target - root.val) + pathTotal(root.right, target - root.val);
+		return pathTotal(root.left, target - root.val) + pathTotal(root.right, target - root.val);
+	}
     
-    public int hasSum(TreeNode root, int sum) {
-    	if(root == null)
-    		return 0;
-    	if(root.val == sum)
-    		return 1;
-    	return hasSum(root.left, sum - root.val) + hasSum(root.right, sum - root.val);	
-    }
+//    
+//    public int pathTotal(TreeNode root, int target){
+//    	if(root == null)
+//    		return 0;
+//    	if(root.val == target)
+//    		return 1 + pathTotal(root.left, target-root.val) + pathTotal(root.right, target-root.val);;
+//    	return pathTotal(root.left, target-root.val) + pathTotal(root.right, target-root.val);
+//    }
+//    
+
 }
