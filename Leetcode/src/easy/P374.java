227dbdd736b814f29be14a26f370c8af00a7299a
JackPaul <479082766@qq.com>
2018-09-03 15:10:14 +0800

    ...

--- Leetcode/src/easy/P374.java
+++ Leetcode/src/easy/P374.java
@@ -1,9 +1,29 @@
 package easy;
 
 public class P374 {
-
-	public P374() {
-		// TODO Auto-generated constructor stub
+	int guess(int num) {
+		return 0;
 	}
+    public int guessNumber(int n) {
+        if(n == 1)
+    		return 1;
+        int low = 1;
+        int high = n;
+        int center = 0;
+        int f;
+        while(low <= high) {
+        	center = (low + high) >>> 1;	
+        	f = guess(center);
+        	if(f == 0)
+        		return center;
+        	if(f == 1) {
+        		low = center + 1;
+        	} else {
+                high = center - 1;
+        	}
+        }
+          
+        return -1;
+    }
 
 }
