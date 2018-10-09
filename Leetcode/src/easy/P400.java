ce5471d4e5ed70cce63f6b0a7f5ed77cb1cddf11
JackPaul <479082766@qq.com>
2018-10-09 20:31:47 +0800

    。。。

--- Leetcode/src/easy/P400.java
+++ Leetcode/src/easy/P400.java
@@ -3,16 +3,14 @@ package easy;
 public class P400 {
 
     public static int findNthDigit(int n) {
-    	int m = n / 9;
-    	int i = 1;
-    	int count = 1;
-    	while(m / i > 1) {
-    		i = i * 10;
-    		count++;
-    	}
-    	System.out.println(count);
-    	System.out.println(i);
-    	return 1;
+    	int res = 0;
+    	
+    	int  i = 1;
+    	int k = 1;
+    	//int res = 0;
+    	
+    	int total = 0;
+    	//while(n > total)
     }
     
     public static void main(String[] args) {
