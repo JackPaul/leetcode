675b0e2332d07f4c29a27bc001ac0ff97900393b
JackPaul <479082766@qq.com>
2018-10-10 15:31:33 +0800

    3

--- Leetcode/src/easy/P400.java
+++ Leetcode/src/easy/P400.java
@@ -6,14 +6,29 @@ public class P400 {
     	int res = 0;
     	
     	int  i = 1;
-    	int k = 1;
-    	//int res = 0;
+    	long k = 1;
     	
-    	int total = 0;
-    	//while(n > total)
+    	long total = 0;
+    	while(n > total) {
+    		total += i * 9 * k;
+    		i++;
+    		k *= 10;
+    	}
+    	
+    	i -= 1;
+    	k /= 10;
+    	total = total - i * 9 * k;
+    	int num = (int) ((k - 1) + (n - total) / i);
+    	//System.out.println(total);
+    	if((n - total) % i != 0) {
+    		num ++;
+    		return (num + "").charAt((int) ((n - total) % i - 1)) - '0';
+    	} else {
+    		return ((num + "").charAt(i-1)) - '0';
+    	}
     }
     
     public static void main(String[] args) {
-		findNthDigit(12);
+		System.out.println(findNthDigit(1000000000));
 	}
 }
