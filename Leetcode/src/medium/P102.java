26edd15985995751bf142c0e79f6e1cf46426d87
Jack Paul <jackpaul.bupt@gmail.com>
2018-10-14 10:50:41 +0800

    ...

---
+++ Leetcode/src/medium/P102.java
@@ -0,0 +1,26 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P102 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+    	List<List<Integer>> res = new ArrayList<>();
+    	
+    	
+    	return res;
+    }
+    
+    
+    public int level(TreeNode root, TreeNode node) {
+    	return helper(root, node, 0);
+    }
+
+
+	private int helper(TreeNode root, TreeNode node, int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
