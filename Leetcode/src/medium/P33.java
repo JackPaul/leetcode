3f42df630462614edb112e0376c9f71f7200767c
JackPaul <479082766@qq.com>
2018-10-16 21:06:33 +0800

    3

---
+++ Leetcode/src/medium/P33.java
@@ -0,0 +1,17 @@
+package medium;
+
+import java.util.Arrays;
+
+public class P33 {
+    public static int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+	private static int binarySearch(int[] nums, int i, int j, int t) {
+
+	}
+	
+	public static void main(String[] args) {
+
+	}
+}
