6f3071480fc10c616d7f2638f3f15a2b07b7ade4
Jack Paul <jackpaul.bupt@gmail.com>
2018-10-19 21:06:34 +0800

    43

--- Leetcode/src/medium/P43.java
+++ Leetcode/src/medium/P43.java
@@ -4,10 +4,10 @@ public class P43 {
     public static String multiply(String num1, String num2) {
         if(num1.length() == 1 && num1.charAt(0) == '0')
         	return "0";
-        if(num1.length() == 1 && num1.charAt(0) == '0')
+        if(num2.length() == 1 && num2.charAt(0) == '0')
         	return "0";
         
-        String res = "0";
+        String res = "";
         String zeroSuffix = "";
         String currentSum="";
         
@@ -18,23 +18,66 @@ public class P43 {
             int a = num2.charAt(i) - '0';
         	for(int j = num1.length()-1; j >= 0; j--) {
         		int b = num1.charAt(j) - '0';
-        		//System.out.println(a+" "+b);
         		int c = (a * b + f) / 10;
         		m = (a * b + f) % 10;
         		f = c;
-        		System.out.println(m);
         		currentSum = m + currentSum;	
         	}
         	if(f > 0)
         		currentSum = f + currentSum;
         	currentSum += zeroSuffix;
-        	System.out.println(currentSum);
         	zeroSuffix += 0;
+        	if(res.equals("")) {
+        		res += currentSum;
+        	} else {
+        		int i1 = res.length() - 1;
+        		int j1 = currentSum.length() - 1;
+        		int flag = 0;
+        		String sum = "";
+        		while(i1 >= 0 && j1 >= 0) {
+        			int a1 = res.charAt(i1) - '0';
+        			int a2 = currentSum.charAt(j1) - '0';
+            		int c = (a1 + a2 + flag) / 10;
+            		m = (a1 + a2 + flag) % 10;
+            		flag = c;
+        			sum = m + sum;
+        			i1--;
+        			j1--;
+        		}
+        		
+        		if(i1 == -1 && j1 == -1) {
+        			if(flag > 0)
+        				sum = flag + sum;
+        		} else if(i1 == -1){
+        			while(j1 >= 0) {
+        				int a2 = currentSum.charAt(j1) - '0';
+        				int c1 = (a2 + flag) / 10;
+                		m = (a2 + flag) % 10;
+                		flag = c1;
+            			sum = m + sum;
+            			j1--;
+        			}
+        			if(flag > 0)
+        				sum = flag + sum;
+        		} else {
+        			while(i1 >= 0) {
+        				int a1 = res.charAt(i1) - '0';
+        				int c1 = (a1 + flag) / 10;
+                		m = (a1 + flag) % 10;
+                		flag = c1;
+            			sum = m + sum;
+            			i1--;
+        			}
+        			if(flag > 0)
+        				sum = flag + sum;
+        		}
+        		res = sum;
+        	}
         }
-        return "";
+        return res;
     }
     
     public static void main(String[] args) {
-		System.out.println(multiply("567", "456"));
+		System.out.println(multiply("123456789", "987654321"));
 	}
 }
