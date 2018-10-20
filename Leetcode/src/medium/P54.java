3f5cf1a4207d14092bcc88728dc81ba7d991231e
JackPaul <479082766@qq.com>
2018-10-20 22:00:19 +0800

    2

--- Leetcode/src/medium/P54.java
+++ Leetcode/src/medium/P54.java
@@ -43,7 +43,6 @@ public class P54 {
 		}
 	}
 	
-	
 	public static void main(String[] args) {
 		int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 		System.out.println(spiralOrder(matrix));
--- Leetcode/src/medium/P56.java
+++ Leetcode/src/medium/P56.java
@@ -33,9 +33,7 @@ public class P56 {
     				flag[i] = true;
     			} else {
     				i--;
-    			}
-    			
-    			
+    			}	
     		}
     	}		
     	return res;
