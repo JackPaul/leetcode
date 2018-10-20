10f83f0c55ba8a9359599ab64f794acbab6cb8f1
JackPaul <479082766@qq.com>
2018-10-20 20:49:19 +0800

    4

---
+++ Leetcode/src/medium/P47.java
@@ -0,0 +1,15 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P47 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+    	//Arrays.sort(nums);
+    	List<List<Integer>> res = new ArrayList<>();
+    	
+    	
+    	return null;
+    }
+}
---
+++ Leetcode/src/medium/P59.java
@@ -0,0 +1,9 @@
+package medium;
+
+public class P59 {
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        
+        return res;
+    }
+}
