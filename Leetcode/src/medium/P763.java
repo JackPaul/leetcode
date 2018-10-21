218f6da837087d9cfc815d0d7497618f679fab91
Jack Paul <jackpaul.bupt@gmail.com>
2018-10-21 23:04:36 +0800

    2

--- Leetcode/src/medium/P763.java
+++ Leetcode/src/medium/P763.java
@@ -8,16 +8,36 @@ public class P763 {
         List<Integer> res = new ArrayList<>();
         
         int beginIndex = 0;
+        int endIndex = 0;
         int tail = S.length() - 1;
-        int len
-        
-        
+        int i = 0;
+        i = beginIndex;
+        while(i < S.length()) {
+        	while(tail > endIndex && S.charAt(tail) != S.charAt(i)) {
+        		tail--;
+        	}
+        	
+        	if(tail == i) {
+        		if(i == endIndex) {
+            		res.add(tail - beginIndex + 1);
+            		beginIndex = tail + 1;
+            		i = beginIndex;
+            		endIndex = i;
+        		}
+        		
+        	}else {
+        		endIndex = tail;
+        		i++;
+        	}
+        	
+        	tail = S.length() - 1;
+        }
         return res;
     }
     
     public static void main(String[] args)
     {
-    	String S = "ababcbacadefegdehijhklij";
+    	String S = "aaa";
 		System.out.println(partitionLabels(S));
 	}
 }
