89c4e943c56c5e99171cf4a4f5e8d120fa4ade41
Jack Paul <jackpaul.bupt@gmail.com>
2018-10-30 07:59:49 +0800

    1.5

--- Leetcode/src/medium/P201.java
+++ Leetcode/src/medium/P201.java
@@ -2,24 +2,14 @@ package medium;
 
 public class P201 {
     public static int rangeBitwiseAnd(int m, int n) {
-    	if(m <= 0 && n >= 0)
+    	if(m == 0 && n == 0)
     		return 0;
     	if(m == n)
     		return m;
-    	if(m == 1)
-    		return 0;
-    	int a = 1;
-    	while(m / a > 1) {
-    		a <<= 1;
-    	}
-    	//System.out.println(a);
-    	if(m <= a)
-    		return a;
-    	
-        return a & rangeBitwiseAnd(m-a, n-a);
+        return 2 * rangeBitwiseAnd(m >> 1, n >> 1);
     }
     
     public static void main(String[] args) {
-		System.out.println(rangeBitwiseAnd(5, 7));
+		System.out.println(rangeBitwiseAnd(2, 3));
 	}
 }
