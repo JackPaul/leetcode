222a85b9fc7c3a4ac06a8f906ef306ef93b1c92b
Jack Paul <jackpaul.bupt@gmail.com>
2018-10-31 21:57:59 +0800

    79

--- Leetcode/src/medium/P79.java
+++ Leetcode/src/medium/P79.java
@@ -5,10 +5,12 @@ import java.util.Map;
 
 public class P79 {
     public static boolean exist(char[][] board, String word) {
+    	if(word.length() > board.length * board[0].length)
+    		return false;
        	for(int i = 0; i < board.length; i++) {
        		for(int j = 0; j < board[0].length; j++) {
        			int[][] visited = new int[board.length][board[0].length];
-       			if(dfs(board, i, j, word, visited))
+       			if(board[i][j] == word.charAt(0) && dfs(board, i, j, word, visited))
        				return true;
        		}
        	}
@@ -16,25 +18,27 @@ public class P79 {
     }
 
 	private static boolean dfs(char[][] board, int i, int j, String word, int[][] visited) {
-//		System.out.println(word+".....***");
+		//		System.out.println(word+".....***");
 		if(word.length() == 0) {
 			return true;
 		}
 			
 		if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(0) || visited[i][j] == 1) {
-//			System.out.println(word+" "+i+"  "+j+"  "+false);
 			return false;
 		}
 				
 		String str = word.substring(1);
 		visited[i][j] = 1;
-		return dfs(board, i+1, j, str, visited) || dfs(board, i-1, j, str, visited) || dfs(board, i, j+1, str, visited) || dfs(board, i, j-1, str, visited);
+		boolean flag = dfs(board, i+1, j, str, visited) || dfs(board, i-1, j, str, visited) || dfs(board, i, j+1, str, visited) || dfs(board, i, j-1, str, visited);
+		if(flag == false) {
+			visited[i][j] = 0;
+		}
+		return flag;
 	}
 
 	public static void main(String[] args) {
-//		char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
-		char[][] board = {{'a','a'}};
+		char[][] board = {{'A','B','C','E'},{'S','F','E','S'},{'A','D','E','E'}};
 				
-		System.out.println(exist(board, "aaa"));
+		System.out.println(exist(board, "ABCESEEEFS"));
 	}
 }
