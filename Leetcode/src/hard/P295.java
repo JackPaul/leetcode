489d9475a0dd62b64c155b246d9dc45619333b04
Jack Paul <jackpaul.bupt@gmail.com>
2019-01-10 08:55:50 +0800

    //

--- Leetcode/src/hard/P295.java
+++ Leetcode/src/hard/P295.java
@@ -3,6 +3,7 @@ package hard;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class P295 {
 
@@ -42,12 +43,18 @@ public class P295 {
 	}
 	
 	public static void main(String[] args) {
-		 MedianFinder obj = new MedianFinder();
-		 obj.addNum(1);
-		 obj.addNum(2);
-		 double param_2 = obj.findMedian();
-		 System.out.println(param_2);
-		 HashMap<K, V>
+//		 MedianFinder obj = new MedianFinder();
+//		 obj.addNum(1);
+//		 obj.addNum(2);
+//		 double param_2 = obj.findMedian();
+//		 System.out.println(param_2);
+		String a = "haha";
+		String b = "haha";
+		String c = new String("haha");
+		System.out.println(a == b);
+		System.out.println(a == c);
+		System.out.println(a.equals(b));
+		System.out.println(a.equals(c));
 	}
 
 
